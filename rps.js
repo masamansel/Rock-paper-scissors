94894d69e57302378d899780c53a5f8df7e50e02
Revolution <94225840+masamansel@users.noreply.github.com>
2024-05-10 02:38:35 +0300

    Display score in DOM

--- rps.js
+++ rps.js
@@ -8,27 +8,29 @@ function getComputerChoice() {
   }
 }
 let playerSelection;
-
+let result = document.getElementById("result");
+let scoreb = document.getElementById("score-board");
+let winRes = document.getElementById("winner");
 let computerSelection = getComputerChoice().toString();
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    console.log("tie");
+    return "tie"
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
-    console.log("Mann wins");
+    return"Mann wins"
   } else if (
     (computerSelection === "rock" && playerSelection === "scissors") ||
     (computerSelection === "scissors" && playerSelection === "paper") ||
     (computerSelection === "paper" && playerSelection === "rock")
   ) {
-    console.log("Machine wins");
+    return "Machine wins"
   }
 }
 
-playerSelection="rock";
+ 
 let rock = document.getElementById("rock");
 let paper = document.getElementById("paper");
 let scissors = document.getElementById("scissors");
@@ -36,21 +38,24 @@ let scissors = document.getElementById("scissors");
 rock.addEventListener("click", (e)=>{
   e.stopPropagation()
   getComputerChoice()
+  playerSelection= "rock"
   computerSelection = getComputerChoice().toString();
-  playRound(playerSelection, computerSelection)
+  game()
   
 });
 paper.addEventListener("click", (e)=>{
   e.stopPropagation()
   getComputerChoice()
+  playerSelection= "paper"
   computerSelection = getComputerChoice().toString();
-  playRound(playerSelection, computerSelection)
+  game()
 });
 scissors.addEventListener("click", (e)=>{
   e.stopPropagation()
   getComputerChoice()
+  playerSelection= "scissors"
   computerSelection = getComputerChoice().toString();
-  playRound(playerSelection, computerSelection)
+  game()
 });
 
 let playerPoints = 0;
@@ -60,24 +65,30 @@ let computerPoints = 0;
 function game() {
   playRound(playerSelection, computerSelection);
 
-  if (playRound(playerSelection, computerSelection) === "You win.") {
+  if (playRound(playerSelection, computerSelection) === "Mann wins") {
     ++playerPoints;
   } else if (
-    playRound(playerSelection, computerSelection) === "Computer wins."
+    playRound(playerSelection, computerSelection) === "Machine wins"
   ) {
     ++computerPoints;
   }
 
-  console.log("Player:", playerPoints, "    ", "Machine:", computerPoints);
-  console.log(
-    playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1),
-    "           ",
-    computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
-  );
-  console.log(playRound(playerSelection, computerSelection));
+  scoreb.innerText= `Mann: ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}\, Mann Points: ${playerPoints} Machine: ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} Machine points: ${computerPoints}`;
+ 
   if (playerPoints == 5) {
-    console.log("Congratulations.You win");
+    winRes.innerText="Victory,";
   } else if (computerPoints == 5) {
-    console.log("Skill issue ngl");
+    winRes.innerText="You failed!"
   }
+}
+if (playerPoints<5||computerPoints<5){
+  for(;;){
+if(playerPoints===5||computerPoints===5){break;}
+    game()
+    
+      
+    }
+}
+else{
+
 }
