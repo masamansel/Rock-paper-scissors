b57e87ea151c1bbbdb9b2808f16787ea2c44c38f
Revolution <94225840+masamansel@users.noreply.github.com>
2024-05-10 02:53:47 +0300

    Bug fixes

--- rps.js
+++ rps.js
@@ -7,17 +7,6 @@ function getComputerChoice() {
     return "paper";
   }
 }
-<<<<<<< HEAD
-
-
-
-
-
-function playRound(playerSelection, computerSelection) {
- 
-  if (playerSelection === computerSelection) {
-    return "It's a draw.";
-=======
 let playerSelection;
 let result = document.getElementById("result");
 let scoreb = document.getElementById("score-board");
@@ -25,123 +14,78 @@ let winRes = document.getElementById("winner");
 let computerSelection = getComputerChoice().toString();
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return "tie"
->>>>>>> rps-ui
+    return "tie";
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
-<<<<<<< HEAD
-    return "You win.";
-  } else if ((computerSelection === "rock" && playerSelection === "scissors") ||
-  (computerSelection === "scissors" && playerSelection === "paper") ||
-  (computerSelection === "paper" && playerSelection === "rock")){
-    return "Computer wins.";
-  } else {
-    return "write rock, paper or scissors";
-  }
-} 
-let playerPoints = 0;
-let computerPoints = 0;
-
-function game() {
-
-  let playerSelection = prompt("Rock, paper, or scissors").toLowerCase();
- 
-let computerSelection = getComputerChoice().toString();
- 
-  playRound(playerSelection, computerSelection)
-
-
-  if (playRound(playerSelection, computerSelection) === "You win.") {
-    ++playerPoints;
-  } else if (
-    playRound(playerSelection, computerSelection) === "Computer wins."
-=======
-    return"Mann wins"
+    return "Mann wins";
   } else if (
     (computerSelection === "rock" && playerSelection === "scissors") ||
     (computerSelection === "scissors" && playerSelection === "paper") ||
     (computerSelection === "paper" && playerSelection === "rock")
   ) {
-    return "Machine wins"
+    return "Machine wins";
   }
 }
 
- 
 let rock = document.getElementById("rock");
 let paper = document.getElementById("paper");
 let scissors = document.getElementById("scissors");
 
-rock.addEventListener("click", (e)=>{
-  e.stopPropagation()
-  getComputerChoice()
-  playerSelection= "rock"
+rock.addEventListener("click", (e) => {
+  e.stopPropagation();
+  getComputerChoice();
+  playerSelection = "rock";
   computerSelection = getComputerChoice().toString();
-  game()
-  
+  game();
 });
-paper.addEventListener("click", (e)=>{
-  e.stopPropagation()
-  getComputerChoice()
-  playerSelection= "paper"
+paper.addEventListener("click", (e) => {
+  e.stopPropagation();
+  getComputerChoice();
+  playerSelection = "paper";
   computerSelection = getComputerChoice().toString();
-  game()
+  game();
 });
-scissors.addEventListener("click", (e)=>{
-  e.stopPropagation()
-  getComputerChoice()
-  playerSelection= "scissors"
+scissors.addEventListener("click", (e) => {
+  e.stopPropagation();
+  getComputerChoice();
+  playerSelection = "scissors";
   computerSelection = getComputerChoice().toString();
-  game()
+  game();
 });
 
 let playerPoints = 0;
 let computerPoints = 0;
-
 
 function game() {
   playRound(playerSelection, computerSelection);
 
   if (playRound(playerSelection, computerSelection) === "Mann wins") {
     ++playerPoints;
-  } else if (
-    playRound(playerSelection, computerSelection) === "Machine wins"
->>>>>>> rps-ui
-  ) {
+  } else if (playRound(playerSelection, computerSelection) === "Machine wins") {
     ++computerPoints;
   }
 
-<<<<<<< HEAD
-  console.log("Player:", playerPoints,'    ',"Machine:", computerPoints);
-  console.log((playerSelection.charAt(0)).toUpperCase() + playerSelection.slice(1),'           ' , computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1))
-  console.log(playRound(playerSelection,computerSelection))
-  if (playerPoints == 5) {
-    console.log("Congratulations.You win");
-  } else if (computerPoints == 5) {
-    console.log("Skill issue ngl");
-  }
-}
+  scoreb.innerText = `Mann: ${
+    playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+  }\, Mann Points: ${playerPoints} Machine: ${
+    computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+  } Machine points: ${computerPoints}`;
 
-=======
-  scoreb.innerText= `Mann: ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}\, Mann Points: ${playerPoints} Machine: ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} Machine points: ${computerPoints}`;
- 
   if (playerPoints == 5) {
-    winRes.innerText="Victory,";
+    winRes.innerText = "Victory,";
   } else if (computerPoints == 5) {
-    winRes.innerText="You failed!"
+    winRes.innerText = "You failed!";
   }
 }
->>>>>>> rps-ui
-if (playerPoints<5||computerPoints<5){
-  for(;;){
-if(playerPoints===5||computerPoints===5){break;}
-    game()
-    
-      
+if (playerPoints < 5 || computerPoints < 5) {
+  for (;;) {
+    if (playerPoints === 5 || computerPoints === 5) {
+      break;
     }
-}
-else{
-
+    game();
+  }
+} else {
 }
